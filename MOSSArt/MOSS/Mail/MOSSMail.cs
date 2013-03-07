00665fe6f754bec5d44d4079e64d688883250957
Porter <wjc1029@gmail.com>
2013-03-07 09:02:15 +0800

    add binary support for serializeUtil

--- MOSSArt/MOSS/Mail/MOSSMail.cs
+++ MOSSArt/MOSS/Mail/MOSSMail.cs
@@ -10,5 +10,10 @@ namespace MOSSArt
         {
             return SPUtility.SendEmail(MOSSContext.Current != null ? MOSSContext.Current.Web : SPContext.Current.Web, true, false, to, subject, messageBody);
         }
+
+        public static bool SendMail(SPWeb web,string to, string subject, string messageBody)
+        {
+            return SPUtility.SendEmail(web, true, false, to, subject, messageBody);
+        }
     }
 }
